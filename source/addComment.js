7e04e9967a5d8a09ce305d6ff57e245ed4c06191
Tyler Westlie <tyler.westlie@gmail.com>
2018-08-12 18:50:50 -0600

    added comment feature

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
   // add your code here
-})
+  $('#new_comment_button').on( 'click', () => {
+    $('#new_comment_button').hide();
+    $('#new_comment').show();
+  });
+
+  $('#new_comment').submit( () => {
+    event.preventDefault();
+
+    let comment = $('textarea[name="comment"]').val()
+    let author = $('input[id="authorName"]').val()
+
+      $('#comment_list').append('<li>' + comment + "<span class='author'>" + author + "</span></li>");
+
+      $('#new_comment_button').show();
+      $('form#new_comment').hide();
+  });
+});
